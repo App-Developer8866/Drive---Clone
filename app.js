2541f401fc3875fdf911a94c90a28c0ce6a8c172
App-Developer8866 <muttrafgujjar.787@gmail.com>
2025-08-21 00:05:12 +0500

    config dotenv to access .env file data

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const userRoutes = require("./routes/userRoutes");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const server = express();
 const PORT = process.env.PORT || 8080;
@@ -12,6 +15,6 @@ server.use(morgan("dev"));
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
-server.use('/user', userRoutes);
+server.use("/user", userRoutes);
 
 server.listen(PORT);
