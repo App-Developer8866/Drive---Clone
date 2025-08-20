e6b674c78c992f4c30930ae2df9e1beb5e49922a
App-Developer8866 <muttrafgujjar.787@gmail.com>
2025-08-20 21:05:57 +0500

    Separate the routes and initiat drive clone

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const userRoutes = require("./routes/userRoutes");
 
 const server = express();
 const PORT = process.env.PORT || 8080;
@@ -11,13 +12,6 @@ server.use(morgan("dev"));
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
-server.get("/", (req, res) => {
-  res.render("index");
-});
-
-server.post("/user-form-data", (req, res) => {
-  console.log("Received user form data:", req.body);
-  res.send("Form data received successfully! " + JSON.stringify(req.body));
-});
+server.use('/user', userRoutes);
 
 server.listen(PORT);
