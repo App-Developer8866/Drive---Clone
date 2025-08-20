07f10461886fd8a2cc5bfcbf54b585e9abcba8ac
App-Developer8866 <muttrafgujjar.787@gmail.com>
2025-08-21 00:08:20 +0500

    create mongoDB connect

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const morgan = require("morgan");
 const userRoutes = require("./routes/userRoutes");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 dotenv.config();
+connectDB();
 
 const server = express();
 const PORT = process.env.PORT || 8080;
