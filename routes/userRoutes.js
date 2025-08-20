83d144df7689263e6230bc5196087a63971f51e7
App-Developer8866 <muttrafgujjar.787@gmail.com>
2025-08-20 21:17:13 +0500

    define user registration validations

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,45 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 
 const router = express.Router();
 
 router.get("/register", (req, res) => {
   res.render("register");
 });
+
+router.post(
+  "/register",
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage("Username is required")
+    .isLength({ min: 3 })
+    .withMessage("Username must be at least 3 characters long"),
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Invalid email address")
+    .isLength({ min: 13 })
+    .withMessage("Email must be at least 13 characters long"),
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long"),
+  (req, res) => {
+    console.log("Received user form data:", req.body);
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .json({ errors: errors.array(), message: "Validation failed" });
+    }
+    res.send("User registration data received successfully!");
+  }
+);
 
 module.exports = router;
