0dfb16376af10332c472480a6d0c2476f9014088
App-Developer8866 <muttrafgujjar.787@gmail.com>
2025-08-21 00:57:39 +0500

    set cookieParser middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const userRoutes = require("./routes/userRoutes");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const cookieParser = require("cookie-parser");
 
 dotenv.config();
 connectDB();
@@ -14,6 +15,9 @@ server.set("view engine", "ejs");
 
 // Middleware to log requests
 server.use(morgan("dev"));
+// Middleware to parse cookies
+server.use(cookieParser());
+// Middleware to parse JSON and URL-encoded data
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
